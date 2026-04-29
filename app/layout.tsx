b3d3ca9d2315e186dda4ac20771ad61d43bd29fd
faseeh <faseeh1080@gmail.com>
2026-04-29 11:50:58 +0000

    add layout styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="w-full flex flex-col items-center p-4">{children}</body>
     </html>
   );
 }
