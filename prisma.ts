c2925c5ba1923be71cd25ed8fc418fd2339a3321
faseeh <faseeh1080@gmail.com>
2026-04-29 09:38:29 +0000

    remove withAccelerate()

--- prisma.ts
+++ prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@/src/generated/prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
@@ -8,8 +7,6 @@ const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({ adapter }).$extends(withAccelerate());
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
